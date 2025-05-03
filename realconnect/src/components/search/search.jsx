d6e4d2f8de2632373694375c353b9ab7322c85b9
Minchaez <rlawlsdl0730@gmail.com>
2025-05-04 04:59:31 +0900

    Feat : 검색 정적 디자인 구현

--- realconnect/src/components/search/search.jsx
+++ realconnect/src/components/search/search.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./search.css";
+import searchIcon from "../../assets/icons/search.svg";
 
 const Search = ({ onSearch }) => {
   const [searchTerm, setSearchTerm] = useState("");
@@ -14,6 +15,9 @@ const Search = ({ onSearch }) => {
   return (
     <div className="search-container">
       <div className="search-input-wrapper">
+        <div className="search-icon left" aria-hidden="true">
+          <img src={searchIcon} alt="" />
+        </div>
         <input
           type="text"
           className="search-input"
@@ -21,23 +25,6 @@ const Search = ({ onSearch }) => {
           value={searchTerm}
           onChange={handleSearch}
         />
-        <div className="search-icon">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-          >
-            <path
-              d="M21 21L16.65 16.65M19 11C19 15.4183 15.4183 19 11 19C6.58172 19 3 15.4183 3 11C3 6.58172 6.58172 3 11 3C15.4183 3 19 6.58172 19 11Z"
-              stroke="#888888"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
-        </div>
       </div>
     </div>
   );
