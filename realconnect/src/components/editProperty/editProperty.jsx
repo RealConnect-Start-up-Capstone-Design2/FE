f0c9906985ea971da3e94b952eb575d73687da25
jwe08 <chlduwls03@naver.com>
2025-05-10 00:07:53 +0900

    feat: editProperty 생성

---
+++ realconnect/src/components/editProperty/editProperty.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./editProperty.css";
+import edit from "../../assets/icons/edit.svg";
+
+const editProperty = () => {
+  return (
+    <div className="edit-property">
+      <img src={edit} alt="edit" className="edit-property-img" />
+      <span className="edit-property-text">프로필 수정</span>
+    </div>
+  );
+};
+
+export default editProperty;
