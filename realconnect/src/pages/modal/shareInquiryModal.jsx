18da83022523b4176e75b0c1ccf1c12b318953cb
Minchaez <rlawlsdl0730@gmail.com>
2025-06-03 20:37:58 +0900

    Fix : 배포 문제 해결 2

--- realconnect/src/pages/modal/shareInquiryModal.jsx
+++ realconnect/src/pages/modal/shareInquiryModal.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Modal from "./modal";
-import "./ShareInquiryModal.css";
+import "./shareInquiryModal.css";
 
 const ShareInquiryModal = ({ isOpen, onClose, onSubmit, inquiry }) => {
   const [formData, setFormData] = useState({
