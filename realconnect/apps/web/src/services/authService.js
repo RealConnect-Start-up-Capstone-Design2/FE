0ef0bad997cd5704ce5a4b2e0c02bddfbfb48184
Minchaez <rlawlsdl0730@gmail.com>
2025-09-12 20:45:19 +0900

    Feat: API 응답 인터셉터를 통한 자동 토큰 갱신 기능 추가

--- realconnect/apps/web/src/services/authService.js
+++ realconnect/apps/web/src/services/authService.js
@@ -17,10 +17,7 @@ export const login = async (username, password) => {
   );
 
   // 액세스 토큰은 Authorization 헤더에서 추출
-  const accessToken = response.headers["authorization"]?.replace(
-    "Bearer ",
-    ""
-  );
+  const accessToken = response.headers["authorization"]?.replace("Bearer ", "");
   // 사용자 이름은 응답 데이터에서 추출
   const responseUsername = response.data.username;
 
@@ -30,4 +27,23 @@ export const login = async (username, password) => {
 
   // 성공 시 토큰과 사용자 정보를 반환
   return { accessToken, username: responseUsername };
-}; 
+};
+
+/**
+ * 액세스 토큰을 재발급받는 함수
+ * @returns {Promise<string>} - 새로운 액세스 토큰
+ */
+export const refreshAccessToken = async () => {
+  const response = await api.get("/refresh-token", {
+    withCredentials: true, // 쿠키(리프레시 토큰) 전송을 위함
+  });
+
+  // 액세스 토큰은 Authorization 헤더에서 추출
+  const accessToken = response.headers["authorization"]?.replace("Bearer ", "");
+
+  if (!accessToken) {
+    throw new Error("새로운 액세스 토큰이 없습니다.");
+  }
+
+  return accessToken;
+};
