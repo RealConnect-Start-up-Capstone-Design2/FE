c62f96d9ba8eb14ca6939c6cd17e5277e0549251
Minchaez <rlawlsdl0730@gmail.com>
2025-10-16 21:26:20 +0900

    chore : darkMode제거

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,6 @@ import type { Config } from "tailwindcss";
 import tailwindcssAnimate from "tailwindcss-animate";
 
 const config = {
-  darkMode: ["class"],
   content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx,mdx}"],
   theme: {
     extend: {
