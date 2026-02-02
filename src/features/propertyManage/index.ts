9fd79010ce363ad1d7d8f608f361e363f62c5bd0
Minchaez <rlawlsdl0730@gmail.com>
2026-02-02 21:18:51 +0900

    refactor : 매물 사이드바 작업 전 레거시 컴포넌트 정리

--- src/features/propertyManage/index.ts
+++ src/features/propertyManage/index.ts
@@ -1,4 +1,2 @@
 export * from "./components/PropertyManageTable";
 export * from "./components/PropertyManagerHeader";
-export * from "./components/blocks/PropertyMemoBlock";
-export * from "./components/blocks/PropertyFloorPlanBlock";
